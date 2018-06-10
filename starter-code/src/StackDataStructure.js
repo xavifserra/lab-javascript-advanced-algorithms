9d5646138f75e509c75a48e1c141296c6de6bcc4
xavifserra <38772006+xavifserra@users.noreply.github.com>
2018-06-10 22:44:21 +0200

    pendiente

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,27 @@
-function StackDataStructure () {
+
+function StackDataStructure() {
+    this.stackControl = [];
+    this.MAX_SIZE = 0;
+    this.isEmpty = function () { return this.stackControl.length === 0; };
 }
+
+StackDataStructure.prototype.canPush = function () {
+    return this.isEmpty();
+};
+
+StackDataStructure.prototype.push = function (element) {
+    if (this.canPush()) {
+        if (this.stackControl.length === this.MAX_SIZE && this.MAX_SIZE != 0) {
+            return "Stack Overflow";
+        }
+        this.stackControl.push(element);
+        return this.stackControl;
+    }
+};
+StackDataStructure.prototype.pop = function () {
+    if (this.stackControl.length === 0) {
+        return "Stack Underflow";
+    }
+    //var popped = this.stackControl.length;
+    return this.stackControl.pop();
+};
